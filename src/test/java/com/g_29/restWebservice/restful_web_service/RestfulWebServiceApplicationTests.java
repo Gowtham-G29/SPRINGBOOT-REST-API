ba738f2e6ba873f4c15415f6299b79a629101277
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-05-16 12:11:22 +0530

    initial commit

---
+++ src/test/java/com/g_29/restWebservice/restful_web_service/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g_29.restWebservice.restful_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
