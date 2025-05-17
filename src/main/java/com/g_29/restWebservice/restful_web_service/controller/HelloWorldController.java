4e9c0b4fc82a45a06111a2743e9941f570300fe4
Gowtham-G29 <gowthamguruvayurappan29@gmail.com>
2025-05-17 23:43:13 +0530

    Internationlization and content negotiations concepts

--- src/main/java/com/g_29/restWebservice/restful_web_service/controller/HelloWorldController.java
+++ src/main/java/com/g_29/restWebservice/restful_web_service/controller/HelloWorldController.java
@@ -1,12 +1,20 @@
 package com.g_29.restWebservice.restful_web_service.controller;
 
 import com.g_29.restWebservice.restful_web_service.model.Helloworld;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Locale;
+
 @RestController
 public class HelloWorldController {
+
+    @Autowired
+    private MessageSource messageSource;
 
 
     @GetMapping("/hello-world")
@@ -22,6 +30,13 @@ public class HelloWorldController {
     @GetMapping("/hello-world/path-variable/{name}")
     public Helloworld helloWorldPathVariable(@PathVariable String name){
          return new Helloworld("Hello world "+name);
+    }
+
+
+    @GetMapping("/hello-world-intl")
+    public String helloWorldInternationalized(){
+        Locale locale= LocaleContextHolder.getLocale();
+        return  messageSource.getMessage("good.morning.message", null,"Default message",locale);
     }
 
 }
